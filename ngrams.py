269172da0b5087be10d415762a3a0b68a8b68ec2
Arne Neumann <github@arne.cl>
2014-10-31 23:39:38 +0100

    simplified ngram generation (list of words only)

--- ngrams.py
+++ ngrams.py
@@ -5,35 +5,33 @@
 from collections import Counter
 
 
-def ngrams(iterable, n, pad=None):
+def ngrams(words, n, pad=None):
     """
-    generates a list of n-gram tuples from the input iterable (e.g.
-    character n-grams if the input is a string or token n-grams if the input
-    is a list of token strings).
+    generates a list of n-gram tuples from the given list of words.
 
     Parameters
     ----------
-    iterable : str or list of str
-        a string (to generate character ngrams) or a list of strings (to
-        generate token ngrams)
+    words : list of str
+        a list of strings
     n : int
         1 to generate unigrams, 2 for bigrams etc.
     pad : str or None
-        If the length of iterable is not evenly divisible by n, the
-        final tuple is dropped if pad is not specified, or filled to length n
-        by pad.
+        If pad is given, it is used to generate ngram tuples for the last
+        n-1 words of the input. For example, in a bigram model, the input
+        ['He', 'died', '.'] would produce the ngrams
+        [('He', 'died'), ('died', '.'), ('.', pad)]. Otherwise, these ngrams
+        would be produced: [('He', 'died'), ('died', '.')].
 
     Returns
     -------
     ngrams : list of tuples of str
     """
     if pad:
-        iterable += pad * n
-        for i in xrange(0, len(iterable)-n):
-            yield tuple(iterable[i:i+n])
-    else:
-        for i in xrange(0, len(iterable)-n+1):
-            yield tuple(iterable[i:i+n])
+        # we need to add the padding element once in a bigram model, twice in a
+        # trigram model etc.
+        words = list(chain(words, repeat(pad, n-1)))
+    for i in xrange(0, len(words)-n+1):
+        yield tuple(words[i:i+n])
 
 
 def ngram_counts(iterable, n, pad=' '):
